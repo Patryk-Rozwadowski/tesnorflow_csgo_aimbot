3b86dfc891abd5e5d33fee193ff0659766fbd46c
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-05-02 20:37:36 +0200

    Create new better model

--- object_detection/settings/object_detection_paths.py
+++ object_detection/settings/object_detection_paths.py
@@ -1,4 +1,4 @@
 paths = {
-    'frozen_graph': 'core/CSGO_inference_graph/frozen_inference_graph.pb',
+    'frozen_graph': 'core/inference_graph/frozen_inference_graph.pb',
     'labels': 'core/label_map.pbtxt'
 }
